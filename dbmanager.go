5412e5f04e26f98cc702ba50848637d9f0b53a5f
Jose <jlparra@bhub.com>
2022-07-23 02:33:09 +0200

    update connection string

--- dbmanager.go
+++ dbmanager.go
@@ -4,8 +4,6 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"math/rand"
-	"strconv"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -19,39 +17,11 @@ type dbManager interface {
 }
 
 func newConnect() *sql.DB {
-	db, err := sql.Open("mysql", "dbForest:dbForest@tcp(localhost:3306)/dbForest")
+	db, err := sql.Open("mysql", "dbforest1:dbforest1@tcp(localhost:3306)/dbforest1")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	return db
-}
-func insertDB(db dbManager) error {
-	err := db.insert()
-	return err
-}
-
-func getDB(db dbManager, id string) error {
-	err := db.get(id)
-	return err
-}
-
-func deleteDB(db dbManager) error {
-	err := db.delete()
-	return err
-}
-
-func (car *Car) insert() error {
-	var db = newConnect()
-	car.Id = car.Brand[1:3] + strconv.Itoa(rand.Intn(1000)) + car.Model[1:3]
-	_, err := db.Query(fmt.Sprintf("INSERT INTO cars VALUES ( '%s' ,'%s','%s',%d );", car.Id, car.Brand, car.Model, car.Horse_power))
-	if err != nil {
-		_, err = db.Query(fmt.Sprintf("UPDATE cars SET brand = '%s' , model = '%s', horse_power = %d  WHERE  id = '%s';", car.Brand, car.Model, car.Horse_power, car.Id))
-		if err != nil {
-			return err
-		}
-	}
-	defer db.Close()
-	return err
 }
 
 func insertZone(z ZoneInData) error {
@@ -68,16 +38,6 @@ func insertZone(z ZoneInData) error {
 		return err
 	}
 
-	defer db.Close()
-	return err
-}
-
-func (car *Car) get(id string) error {
-	var db = newConnect()
-	err := db.QueryRow("SELECT id,brand,model,horse_power FROM cars WHERE id = ?", id).Scan(&car.Id, &car.Brand, &car.Model, &car.Horse_power)
-	if err != nil {
-		return err
-	}
 	defer db.Close()
 	return err
 }
@@ -115,16 +75,6 @@ func getDataZone(zone string, types string) ([]DataForest, error) {
 	}
 	defer db.Close()
 	return data, err
-}
-
-func (car *Car) delete() error {
-	var db = newConnect()
-	_, err := db.Query(fmt.Sprintf("DELETE FROM cars WHERE id = '%s'", car.Id))
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-	return err
 }
 
 func (auth *Auth) get(id string) error {
